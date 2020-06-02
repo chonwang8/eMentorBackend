3b1343363c0ac3eab370e2784c4e96db1d364cb4
ChonWang8 <chonwang8@gmail.com>
2020-06-02 20:48:22 +0700

    Add project files.

---
+++ eMentor.Data/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.UnitOfWork.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IBaseRepository<T> GetRepository<T>() where T : class;
+
+        void Commit();
+    }
+}
---
+++ eMentor.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Data.Repositories.Interfaces;
+using Data.UnitOfWork.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBaseRepository<T> GetRepository<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
