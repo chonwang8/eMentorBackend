ee39eb6e4014868a00bfab93ee953e37005d06a3
ChonWang8 <chonwang8@gmail.com>
2020-06-10 08:35:44 +0700

    add basic methods for SharingModel

---
+++ Domain/Services/Interfaces/ISharingService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Services.Interfaces
+{
+    public interface ISharingService
+    {
+        public IEnumerable<SharingViewModel> GetAll();
+        public IEnumerable<SharingViewModel> GetById(string sharingId);
+        public int Insert(SharingViewModel sharingViewModel);
+        public int Update(SharingViewModel sharingViewModel);
+        public int ChangeStatus(string sharingId, bool status);
+        public int Delete(string sharingId);
+    }
+}
