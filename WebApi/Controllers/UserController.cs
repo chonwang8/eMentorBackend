e9a9163ec7e0557469af7d21d7b835842569dc8d
ChonWang8 <chonwang8@gmail.com>
2020-06-12 15:30:39 +0700

    update user api comment

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -19,18 +19,36 @@ namespace WebApi.Controllers
             _user = service;
         }
 
-
+        /// <summary>
+        /// View list of users
+        /// </summary>
+        /// <returns>List of users</returns>
+        /// <response code="200">List of challenge</response>
+        /// <response code="400">Not have enough infomation</response>
+        /// <response code="401">Unauthorize</response>
+        /// <response code="403">Forbidden from resource</response>
+        /// <response code="404">Empty challenge list</response>
+        /// <response code="500">Internal Error</response>
         [HttpGet]
         public IActionResult GetAll()
         {
             List<UserViewModel> result = _user.GetAll().ToList();
             if (result == null || result.Count == 0)
             {
-                return Ok("There are no users in the system");
+                return NotFound("There are no users in the system");
             }
             return Ok(result);
         }
 
+
+        /// <summary>
+        /// Return User with matching Id
+        /// </summary>
+        /// <returns>User with matching Id</returns>
+        /// <response code="200">User found with matching Id</response>
+        /// <response code="400">Invalid input</response>
+        /// <response code="404">User with matching Id not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpGet("{userId}")]
         public IActionResult GetById(string userId)
         {
@@ -49,6 +67,14 @@ namespace WebApi.Controllers
             return Ok(result);
         }
 
+
+        /// <summary>
+        /// Insert a user into database
+        /// </summary>
+        /// <returns>Query status</returns>
+        /// <response code="200">User successfully inserted</response>
+        /// <response code="400">Invalid input</response>
+        /// <response code="500">Internal server error</response>
         [HttpPost]
         public IActionResult Insert(UserViewModel user)
         {
@@ -71,6 +97,15 @@ namespace WebApi.Controllers
             return Ok("Insert user " + user.Email);
         }
 
+
+        /// <summary>
+        /// Update an existing user
+        /// </summary>
+        /// <returns>Query status</returns>
+        /// <response code="200">User successfully updated</response>
+        /// <response code="400">Invalid input</response>
+        /// <response code="404">User with matching Id not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpPut]
         public IActionResult Update(UserViewModel user)
         {
@@ -93,6 +128,15 @@ namespace WebApi.Controllers
             return Ok("Updated user " + user.Email);
         }
 
+
+        /// <summary>
+        /// Disable a user
+        /// </summary>
+        /// <returns>Query status</returns>
+        /// <response code="200">Disabled user with matching Id</response>
+        /// <response code="400">Invalid input</response>
+        /// <response code="404">User with matching Id not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpPut("status")]
         public IActionResult Disable(UserStatusViewModel user)
         {
@@ -115,6 +159,15 @@ namespace WebApi.Controllers
             return Ok("User is disabled.");
         }
 
+
+        /// <summary>
+        /// Delete a user from database
+        /// </summary>
+        /// <returns>Query status</returns>
+        /// <response code="200">User was deleted from database</response>
+        /// <response code="400">Invalid input</response>
+        /// <response code="404">User with matching Id not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpDelete("{id}")]
         public IActionResult Delete(string userId)
         {
@@ -151,6 +204,11 @@ namespace WebApi.Controllers
         }
 
 
+        /// <summary>
+        /// Register a user to database
+        /// </summary>
+        /// <returns>Registered the user</returns>
+        /// <response code="200">Success</response>
         [HttpPost]
         public IActionResult Register(UserRegisterViewModel user)
         {
@@ -170,6 +228,11 @@ namespace WebApi.Controllers
             _service = service;
         }
 
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <returns>User with matching Id</returns>
+        /// <response code="200">Success</response>
         [HttpPost]
         public IActionResult Login(UserLoginViewModel user)
         {
