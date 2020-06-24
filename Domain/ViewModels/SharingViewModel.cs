eb7cdea6eb4905e90ec9c6e7c1adda0670b711ec
ChonWang8 <chonwang8@gmail.com>
2020-06-24 09:17:09 +0700

    Modify sharing return model

--- Domain/ViewModels/SharingViewModel.cs
+++ Domain/ViewModels/SharingViewModel.cs
@@ -15,4 +15,19 @@ namespace Domain.ViewModels
         public double Price { get; set; }
         public Guid ChannelId { get; set; }
     }
+
+    public class SharingInfoViewModel
+    {
+        public Guid SharingId { get; set; }
+        public string SharingName { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Maximum { get; set; }
+        public double Price { get; set; }
+        public Guid ChannelId { get; set; }
+        public string imageUrl { get; set; }
+        public string TopicName { get; set; }
+
+    }
 }
