a6203da2d9ffef93f804926ce0fc560f3ecde9ea
Piim Trần <piimtran@gmail.com>
2020-07-06 23:24:02 +0700

    EditGetChannelFunction

--- Domain/ViewModels/ChannelModels/GetChannelViewModel.cs
+++ Domain/ViewModels/ChannelModels/GetChannelViewModel.cs
@@ -5,7 +5,7 @@ namespace Domain.ViewModels.ChannelModels
     public class GetChannelViewModel
     {
         public Guid ChannelId { get; set; }
-        public Guid TopicId { get; set; }
-        public Guid MentorId { get; set; }
+        public string TopicName { get; set; }
+        public string MentorName { get; set; }
     }
 }
