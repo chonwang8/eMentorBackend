f6d96a63497cd620ff6cf0324faf8f86a46eb8e6
ChonWang8 <chonwang8@gmail.com>
2020-07-21 11:32:11 +0700

    fix enroll apis

--- Domain/Models/EnrollModels/EnrollViewModel.cs
+++ Domain/Models/EnrollModels/EnrollViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Models.SharingModels;
+using Domain.Models.SubscriptionModels;
+using System;
 
 namespace Domain.Models.EnrollModels
 {
@@ -8,6 +10,8 @@ namespace Domain.Models.EnrollModels
         public Guid SubscriptionId { get; set; }
         public Guid SharingId { get; set; }
         public string SharingName { get; set; }
+        public string MenteeName { get; set; }
+        public string MentorName { get; set; }
         public bool IsDisable { get; set; }
     }
 }
